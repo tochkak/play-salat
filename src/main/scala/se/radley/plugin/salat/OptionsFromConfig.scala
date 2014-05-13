3c91388dbdbdc5007bc821d01b26ed49f1a21820
Jorrit Posthuma <j.posthuma@gmail.com>
2014-05-13 13:47:05 +0200

    Remove deprecated options
    No "autoConnectRetry" and "maxAutoConnectRetryTime" anymore

--- src/main/scala/se/radley/plugin/salat/OptionsFromConfig.scala
+++ src/main/scala/se/radley/plugin/salat/OptionsFromConfig.scala
@@ -23,14 +23,12 @@ object OptionsFromConfig {
 
     val builder = new MongoClientOptions.Builder()
 
-    config.getBoolean("autoConnectRetry").map(v => builder.autoConnectRetry(v))
     config.getInt("connectionsPerHost").map(v => builder.connectionsPerHost(v))
     config.getInt("connectTimeout").map(v => builder.connectTimeout(v))
     config.getBoolean("cursorFinalizerEnabled").map(v => builder.cursorFinalizerEnabled(v))
     config.getString("dbDecoderFactory").flatMap(className => getInstanceFromName[DBDecoderFactory](className)).map(v => builder.dbDecoderFactory(v))
     config.getString("dbEncoderFactory").flatMap(className => getInstanceFromName[DBEncoderFactory](className)).map(v => builder.dbEncoderFactory(v))
     config.getString("description").map(v => builder.description(v))
-    config.getLong("maxAutoConnectRetryTime").map(v => builder.maxAutoConnectRetryTime(v))
     config.getInt("maxWaitTime").map(v => builder.maxWaitTime(v))
     config.getString("readPreference").flatMap { name =>
       try {
