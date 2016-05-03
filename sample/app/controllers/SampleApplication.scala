e3414afb142c9d72fe26caa906ed8bd0ffbe665a
Shayan Shahand <shayanlinux@users.noreply.github.com>
2016-05-03 17:24:24 +0200

    rename Application to SampleApplication

--- sample/app/controllers/SampleApplication.scala
+++ sample/app/controllers/SampleApplication.scala
@@ -9,16 +9,15 @@ import com.mongodb.casbah.Imports._
 import com.novus.salat._
 
 @Singleton
-class Application @Inject() extends Controller {
+class SampleApplication @Inject() (userDAO: UserDAO) extends Controller {
 
   def list() = Action {
-    val users = User.findAll
+    val users = userDAO.findAll
     Ok(views.html.list(users))
   }
 
   def view(id: ObjectId) = Action {
-    User.findOneById(id).map( user =>
-      Ok(views.html.user(user))
-    ).getOrElse(NotFound)
+    userDAO.findOneById(id).map(user =>
+      Ok(views.html.user(user))).getOrElse(NotFound)
   }
 }
